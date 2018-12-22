5ac31c50e7fb7608b7d033d1ae6d51b30239a1c0
我吃地瓜 <we@t.cc>
2018-12-22 20:32:25 +0800

    Update math.js

--- math.js
+++ math.js
@@ -1,5 +1,6 @@
 const math = require('mathjs')
 
+//四舍五入精度有问题，尾数会出现3分这样的错误
 var fixedNumber = function (num, pre=2) {
 	if (typeof num !== 'number') num = Number(num)
 	if (isNaN(num)) num = 0
@@ -10,3 +11,13 @@ var fixedNumber = function (num, pre=2) {
 	precision = Math.pow(10,pre)
 	return Number((Math.round(fixedNum * precision) / precision).toFixed(pre))
 }
+
+// toFixed 修复
+function toFixed(num, s) {
+    var times = Math.pow(10, s)
+    var des = num * times + 0.5
+    des = parseInt(des, 10) / times
+    return des + ''
+}
+//16位大数，计算错误
+console.log(9999999999999999 == 10000000000000001)  //true
